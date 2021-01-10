ac7e359db9626e11dcfea493b1b7cf0c135a5fb1
Dilip Nandakumar <diliplife29@gmail.com>
2021-01-10 18:55:53 +0530

    Added make commands for clean, test, fmt

--- Makefile
+++ Makefile
@@ -1,8 +1,22 @@
 include ./makeUtils.mk
 
-build:
-	$(call build.local)
+compile:
+	$(call build.compile_local)
+
+clean:
+	$(call build.clean)
+
+test:
+	$(call test.run)
+
+check-fmt:
+	$(call fmt.check)
+
+fix-fmt:
+	$(call fmt.fix)
+
+build: clean check-fmt compile test
 
 build-docker-image:
-	$(call build.linux)
+	$(call build.compile_linux)
 	$(call docker.build_image)
--- makeUtils.mk
+++ makeUtils.mk
@@ -2,13 +2,32 @@ name := go-vanilla-bootstrap
 build_out := out/build
 bin := $(build_out)/$(name)
 linux_bin := $(build_out)/$(name)-linux
+main_dir := ./
+main_dir_files := ./...
 
-define build.local
-	go build -o $(bin) ./
+define build.compile_local
+	go build -o $(bin) $(main_dir)
 endef
 
-define build.linux
-	GOOS=linux go build -o $(linux_bin) ./ 
+define build.compile_linux
+	GOOS=linux go build -o $(linux_bin) $(main_dir)
+endef
+
+define build.clean
+	go clean -cache -testcache $(main_dir)
+endef
+
+define test.run
+	go test -coverpkg=$(main_dir_files) -coverprofile c.out $(main_dir_files)
+	go tool cover -func c.out
+endef
+
+define fmt.check
+	gofmt -d -l -e $(main_dir)
+endef
+
+define fmt.fix
+	gofmt -d -l -w $(main_dir)
 endef
 
 define docker.build_image
